12e4c75ef500bec232823776b4bb4d95f07b53fb
SandraMM1054 <sandramendez@mail.webwe.edu>
2025-09-23 17:04:13 -0600

    agregando archivos

---
+++ codigo3.py
@@ -0,0 +1,3 @@
+# TODO: Usa el código del archivo `codigo2.py` como base para crear una figura diferente.
+# Algunas ideas: un arco, un paralelogramo, una estrella, o cualquier otra figura creativa.
+
