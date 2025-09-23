0ff9f8802dfac9edac4f2f17e8bf1c4433513b69
SandraMM1054 <sandramendez@mail.webwe.edu>
2025-09-23 17:12:39 -0600

    agregando tortuga

--- bucles.py
+++ bucles.py
@@ -1,6 +1,6 @@
 #TODO 1: Escriba un bucle (for loop) que imprima los números de 0 a 5 (inclusive).
 #Pista: Para que incluya el numero 5
-for i in range("O, 6") :
+for i in range(0,6):
     print(i)
 
 #TODO 2: Escriba un bucle for que imprima la frase "Soy " tres veces
@@ -9,5 +9,5 @@ for i in range(3) :
     print("soy")
 
 #TODO 3: Escriba un bucle (for loop) que imprima los números pares del 2 - 10 (inclusivo). #Pista: usa range() para contar de dos en dos.     
-for i in range ("2, 11, 2"):
+for i in range(2, 11, 2):
     print (i)
