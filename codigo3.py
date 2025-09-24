c1ee073a4512b7891a877e74bfed252fcb2548b6
SandraMM1054 <sandramendez@mail.webwe.edu>
2025-09-24 00:48:42 -0600

    terminando 3 estrella

--- codigo3.py
+++ codigo3.py
@@ -1,3 +1,15 @@
 # TODO: Usa el código del archivo `codigo2.py` como base para crear una figura diferente.
 # Algunas ideas: un arco, un paralelogramo, una estrella, o cualquier otra figura creativa.
 
+import turtle
+
+t = turtle.Turtle()
+t.color("blue")
+t.speed(5)
+
+for i in range(5):
+    t.forward(100)
+    t.right(144)
+
+turtle.done()
+
